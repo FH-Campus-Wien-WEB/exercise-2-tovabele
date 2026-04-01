8bc871ae41e348e4410fe09d04983bb433e1bfc9
tova bele <tova.bele@stud.hcw.ac.at>
2026-04-02 00:47:02 +0200

    fixed movies formatting

--- server/movie-model.js
+++ server/movie-model.js
@@ -1,27 +1,25 @@
 /* Task 1.1. Add your movie data here 
    and export it so it's available in server.js */
 
-const movies = [
+const movies = {
+   tt0083658 :
    {
-      tt0083658:
-      {
-         "imdbID": "tt0083658",
-         "Title": "Blade Runner",
-         "Runtime": 117,
-         "Released": "1982-06-25",
-         "Genres": ["Action", "Drama", "Sci - Fi"],
-         "Directors": ["Ridley Scott"],
-         "Writers": ["Hampton Fancher", "David Webb Peoples", "Philip K. Dick"],
-         "Actors": ["Harrison Ford", "Rutger Hauer", "Sean Young"],
-         "Plot": "A blade runner must pursue and terminate four replicants who stole a ship in space and have returned to Earth to find their creator.",
-         "Poster": "https://m.media-amazon.com/images/M/MV5BOWQ4YTBmNTQtMDYxMC00NGFjLTkwOGQtNzdhNmY1Nzc1MzUxXkEyXkFqcGc@._V1_SX300.jpg",
-         "Metascore": 84,
-         "imdbRating": 8.1
-      }
-   },
+      "imdbID": "tt0083658",
+      "Title": "Blade Runner",
+      "Runtime": 117,
+      "Released": "1982-06-25",
+      "Genres": ["Action", "Drama", "Sci - Fi"],
+      "Directors": ["Ridley Scott"],
+      "Writers": ["Hampton Fancher", "David Webb Peoples", "Philip K. Dick"],
+      "Actors": ["Harrison Ford", "Rutger Hauer", "Sean Young"],
+      "Plot": "A blade runner must pursue and terminate four replicants who stole a ship in space and have returned to Earth to find their creator.",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BOWQ4YTBmNTQtMDYxMC00NGFjLTkwOGQtNzdhNmY1Nzc1MzUxXkEyXkFqcGc@._V1_SX300.jpg",
+      "Metascore": 84,
+      "imdbRating": 8.1
+   }
+   ,
 
-   {
-      tt0248845:
+         tt0248845 :
       {
          "imdbID": "tt0248845",
          "Title": "Hedwig and the Angry Inch",
@@ -36,9 +34,9 @@ const movies = [
          "Metascore": 85,
          "imdbRating": 7.7
       }
-   },
-   {
-      tt0887912: 
+      ,
+
+      tt0887912:
       {
          "imdbID": "tt0887912",
          "Title": "The Hurt Locker",
@@ -53,6 +51,5 @@ const movies = [
          "Metascore": 95,
          "imdbRating": 7.5,
       }
-   }
-]
+}
 module.exports.movies = movies;
