f30e92a37bd030978d93a62278bd044855e6b00c
tova bele <tova.bele@stud.hcw.ac.at>
2026-04-04 13:24:36 +0200

    updates index and css

--- server/files/index.js
+++ server/files/index.js
@@ -1,87 +1,99 @@
 window.onload = function () {
   const xhr = new XMLHttpRequest();
 
-  function interpretJSON(j) {
-    const data = JSON.parse(j);
-    const body = document.body;
+  function generateUnorderedList(div, title, entries) {
+    const header = document.createElement('h1')
+    header.textContent = title
+    const list = document.createElement('ul')
+    list.classList.add('unsortedlist')
 
+    entries.forEach(item => {
+      const listItem = document.createElement('li')
+      listItem.classList.add('listitem')
+      listItem.textContent = item
+      list.append(listItem)
+    })
 
-    //looping through the json movie objects
-    data
-    return body
+    div.append(header, list)
   }
-
 
   xhr.onload = function () {
     const bodyElement = document.querySelector("body");
     if (xhr.status == 200) {
       const movies = JSON.parse(xhr.responseText);
+
+      //looping through the json movie objects
       for (const movie of movies) {
-        console.log("stepping through movies array")
+        //console.log("stepping through movies array")
 
         //horrible exercise 1 code here:
-        //movie.forEach(item => {
-        const ratingList = document.createElement('ul')
-        ratingList.id = "ratings"
-        bodyElement.append(ratingList)
+        const article = document.createElement('article')
+        article.classList.add('article')
+
+        //ratings on top
+        const ratingList = document.createElement('p')
+        ratingList.classList.add('ratings')
+        const metascore = document.createElement('span')
+        metascore.textContent = 'Metacritics: ' + movie.Metascore + '%'
+        const imdbrating = document.createElement('span')
+        imdbrating.textContent = 'IMDB: ' + movie.imdbRating + '/10'
+        ratingList.append(metascore, imdbrating)
 
         const poster = document.createElement('img')
-        bodyElement.append(poster)
         poster.src = movie.Poster
-        console.log(movie.Poster)
-        const header = document.createElement('h1')
-        bodyElement.append(header)
-        header.innerText = movie.Title
-        console.log(movie.Title)
-        const releaseList = document.createElement('ul')
-        releaseList.id = "releaseList"
-        bodyElement.append(releaseList)
 
+        const titleArea = document.createElement('div')
+        const header = document.createElement('h1')
+        header.innerText = movie.Title
+
+        const editButton = document.createElement('button')
+        editButton.classList.add('editbutton')
+        editButton.textContent = 'Edit'
+
+        titleArea.append(header,editButton)
+
+        const releaseList = document.createElement('span')
+        releaseList.classList.add('releaseList')
         const runtime = document.createElement('li')
         const hours = Math.floor(movie.Runtime / 60)
         const mins = movie.Runtime % 60
         runtime.textContent = 'Runtime: ' + hours + 'h ' + mins + 'm'
-        releaseList.append(runtime)
 
         const releasedOn = document.createElement('li')
         releasedOn.textContent = 'Released on ' + movie.Released
-        releaseList.append(releasedOn)
 
-        const genres = document.createElement('div')
+        const genresDiv = document.createElement('div')
         // add flexbox to genre css class
-        genres.classList.add("horizontal-genres-container")
+        genresDiv.classList.add("horizontal-genres-container")
+        //for (const genre of Genres){
         movie.Genres.forEach(genre => {
           const span = document.createElement("span")
           span.textContent = genre
-          genres.appendChild(span)
+          span.classList.add("genre")
+          genresDiv.appendChild(span)
         })
-        bodyElement.append(genres)
-        //})
 
-        generateUnorderedList(body, "Director", movie.Directors)
-        generateUnorderedList(body, "Writer", movie.Writers)
-        generateUnorderedList(body, "Actor", movie.Actors)
+        generateUnorderedList(article, "Director", movie.Directors)
+        generateUnorderedList(article, "Writer", movie.Writers)
+        generateUnorderedList(article, "Actor", movie.Actors)
 
         const plot = document.createElement('div')
         plot.textContent = movie.Plot
-        bodyElement.append(plot)
 
-
-        const metascore = document.createElement('li')
-        metascore.textContent = 'Metacritics: ' + movie.Metascore + '%'
-        ratingList.append(metascore)
-
-        const imdbrating = document.createElement('li')
-        imdbrating.textContent = 'IMDB: ' + movie.imdbRating + '/10'
-        ratingList.append(imdbrating)
-        bodyElement.append(ratingList)
-
-        const editButton = document.createElement(editButton)
-        bodyElement.append(editButton)
         //})
         /* Task 1.3. Add your code from exercise 1 here 
-           and include a non-functional 'Edit' button
-           to pass this test */
+        and include a non-functional 'Edit' button
+        to pass this test */
+
+        article.append(ratingList)
+        bodyElement.append(poster)
+        bodyElement.append(titleArea)
+        bodyElement.append(releaseList)
+        releaseList.append(runtime)
+        releaseList.append(releasedOn)
+        article.append(genresDiv)
+        article.append(plot)
+        bodyElement.append(article)
       }
 
     } else {
