03e0cdfbc0996cf4d77f2e73ae314ceab364548d
tova bele <tova.bele@stud.hcw.ac.at>
2026-04-04 12:53:25 +0200

    added test data

--- server/movie-model.js
+++ server/movie-model.js
@@ -1,8 +1,23 @@
 /* Task 1.1. Add your movie data here 
-   and export it so it's available in server.js */
+and export it so it's available in server.js */
 
+/* const movies = {
+   tt0083658: {
+      "Title": "eins",
+      "Genres": ["Action", "Drama", "Sci - Fi"]
+   },
+   tt0248845: {
+      "Title": "zwei",
+      "Genres": ["Action", "Drama", "Sci - Fi"]
+   },
+   tt0887912: {
+      "Title": "drei",
+      "Genres": ["Action", "Drama", "Sci - Fi"]
+   }
+}
+ */
 const movies = {
-   tt0083658 :
+   tt0083658:
    {
       "imdbID": "tt0083658",
       "Title": "Blade Runner",
@@ -19,37 +34,39 @@ const movies = {
    }
    ,
 
-         tt0248845 :
-      {
-         "imdbID": "tt0248845",
-         "Title": "Hedwig and the Angry Inch",
-         "Runtime": 95,
-         "Released": "2001-08-31",
-         "Genres": ["Comedy", "Drama", "Music"],
-         "Directors": ["John Cameron Mitchell"],
-         "Writers": ["John Cameron Mitchell", "Stephen Trask"],
-         "Actors": ["John Cameron Mitchell", "Miriam Shor", "Stephen Trask"],
-         "Plot": "A gender-queer punk-rock singer from East Berlin tours the U.S. with her band as she tells her life story and follows the former lover/band-mate who stole her songs.",
-         "Poster": "https://m.media-amazon.com/images/M/MV5BN2Q4MGJmYTgtNDhiMS00MTcwLTlhZjgtN2U5NWUyYzFiMWRmXkEyXkFqcGc@._V1_SX300.jpg",
-         "Metascore": 85,
-         "imdbRating": 7.7
-      }
-      ,
+   tt0248845:
+   {
+      "imdbID": "tt0248845",
+      "Title": "Hedwig and the Angry Inch",
+      "Runtime": 95,
+      "Released": "2001-08-31",
+      "Genres": ["Comedy", "Drama", "Music"],
+      "Directors": ["John Cameron Mitchell"],
+      "Writers": ["John Cameron Mitchell", "Stephen Trask"],
+      "Actors": ["John Cameron Mitchell", "Miriam Shor", "Stephen Trask"],
+      "Plot": "A gender-queer punk-rock singer from East Berlin tours the U.S. with her band as she tells her life story and follows the former lover/band-mate who stole her songs.",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BN2Q4MGJmYTgtNDhiMS00MTcwLTlhZjgtN2U5NWUyYzFiMWRmXkEyXkFqcGc@._V1_SX300.jpg",
+      "Metascore": 85,
+      "imdbRating": 7.7
+   }
+   ,
 
-      tt0887912:
-      {
-         "imdbID": "tt0887912",
-         "Title": "The Hurt Locker",
-         "Runtime": 131,
-         "Released": "2009-07-31",
-         "Genres": ["Drama", "Thriller", "War"],
-         "Directors": "Kathryn Bigelow",
-         "Writers": "Mark Boal",
-         "Actors": ["Jeremy Renner", "Anthony Mackie", "Brian Geraghty"],
-         "Plot": "During the Iraq War, a Sergeant recently assigned to an army bomb squad is put at odds with his squad mates due to his maverick way of handling his work.",
-         "Poster": "https://m.media-amazon.com/images/M/MV5BNzgyMGM2YTItYzY2Yi00NDQ0LWE0M2EtMGUzYjFlMDgyY2M3XkEyXkFqcGc@._V1_SX300.jpg",
-         "Metascore": 95,
-         "imdbRating": 7.5,
-      }
+   tt0887912:
+   {
+      "imdbID": "tt0887912",
+      "Title": "The Hurt Locker",
+      "Runtime": 131,
+      "Released": "2009-07-31",
+      "Genres": ["Drama", "Thriller", "War"],
+      "Directors": "Kathryn Bigelow",
+      "Writers": "Mark Boal",
+      "Actors": ["Jeremy Renner", "Anthony Mackie", "Brian Geraghty"],
+      "Plot": "During the Iraq War, a Sergeant recently assigned to an army bomb squad is put at odds with his squad mates due to his maverick way of handling his work.",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNzgyMGM2YTItYzY2Yi00NDQ0LWE0M2EtMGUzYjFlMDgyY2M3XkEyXkFqcGc@._V1_SX300.jpg",
+      "Metascore": 95,
+      "imdbRating": 7.5,
+   }
 }
+
 module.exports.movies = movies;
+//console.log(module)
